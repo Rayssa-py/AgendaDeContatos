ef87024c2c8b29ea52632f763fea7d0e490d2dd2
Rayssa Emelly Moura da Silva <rayssaemelly20@gmail.com>
2026-09-15 09:06:20 -0300

    Agenda de contatos v1.1.1 - correção de bug e adicionando agenda.java e ajuda.java

---
+++ src/br/edu/principal/ajuda.java
@@ -0,0 +1,42 @@
+package br.edu.principal;
+
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class ajuda {
+	public static void mostraMenu() {
+    	System.out.println();
+        System.out.println("1 - Adicionar contato");
+        System.out.println("2 - Listar contatos");
+        System.out.println("3 - Procurar contato");
+        System.out.println("4 - Alterar contato");
+        System.out.println("5 - Excluir contato");
+        System.out.println("6 - Sair");
+        System.out.println("7 - Sobre");
+    }
+    
+    public static int selecionaOpcao(Scanner sc) {
+    	System.out.print("Escolha uma opção: ");
+        int opc = sc.nextInt();
+        sc.nextLine();
+        return opc;
+    }
+    
+    
+    public static boolean sair() {
+    	System.out.println("Saindo da Agenda de Contatos...");
+        return false;
+    }
+public static void mostraInicializacao() {
+	System.out.println("==========================");
+    System.out.println("     AGENDA DE CONTATOS    ");
+    System.out.println("          v1.0.0           ");
+    System.out.println("==========================");
+    System.out.println("Bem-vindo!");
+}
+public static void sobre() {
+	JOptionPane.showMessageDialog(null,"Desenvolvido por Rayssa! ;)");
+}
+
+}
